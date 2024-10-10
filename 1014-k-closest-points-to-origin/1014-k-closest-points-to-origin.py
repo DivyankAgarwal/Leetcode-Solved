c27bc970108c94a4b99cb72183539b6a607a904b
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-10 12:11:44 -0400

    Time: 631 ms (20.61%), Space: 23.1 MB (21.12%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -4,12 +4,24 @@ import math
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
+        #brute
 
+        
+        # points_with_distances = [(point[0]**2 + point[1]**2, point) for point in points]
+        
+        # points_with_distances.sort(key=lambda x: x[0])
+        
+        # result = [point for _, point in points_with_distances[:k]]
+        
+        # return result
+
+
+        #better
         def calculate_dist(x1, x2):
             orix = 0
             oriy = 0
 
-            ans = math.sqrt( (x1-orix)**2 + (x2 - oriy)**2 )
+            ans = ((x1-orix)**2 + (x2 - oriy)**2)
 
             return ans
 
@@ -19,20 +31,22 @@ class Solution:
 
             ans = calculate_dist(point[0],point[1])
 
-            heapq.heappush(min_heap, [-ans,i])
+            # heapq.heappush(min_heap, [-ans,i])
+            heapq.heappush(min_heap, [-ans,point])
 
             if len(min_heap) > k:
                 heapq.heappop(min_heap)
 
         print(min_heap)
 
-        result = []
 
-        while k > 0:
-            index = min_heap[0][1]
-            heapq.heappop(min_heap)
-            result.append(points[index])
-            k-=1
+        # while k > 0:
+        #     index = min_heap[0][1]
+        #     heapq.heappop(min_heap)
+        #     result.append(points[index])
+        #     k-=1
+
+        result = [point for _, point in min_heap]
 
         return result
 
