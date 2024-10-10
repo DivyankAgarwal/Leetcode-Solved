97fdab2cae20ab4461df945bf7655c737f92b5e2
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-09 23:32:49 -0400

    Time: 42 ms (13.9%), Space: 16.4 MB (97.21%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -3,25 +3,48 @@ class Solution:
 
         #brute
 
-        ans = 0
+        # ans = 0
 
-        def get_largest_stone():
+        # def get_largest_stone():
 
-            stone1 = stones.index(max(stones))
+        #     stone1 = stones.index(max(stones))
 
-            return stones.pop(stone1)
+        #     return stones.pop(stone1)
 
             
         
-        while len(stones) > 1:
-            stone2 = get_largest_stone()
-            stone1 = get_largest_stone()
+        # while len(stones) > 1:
+        #     stone2 = get_largest_stone()
+        #     stone1 = get_largest_stone()
 
-            if stone1 != stone2:
-                stones.append(-stone1 + stone2)
+        #     if stone1 != stone2:
+        #         stones.append(-stone1 + stone2)
+
+        # if stones:
+        #     return stones[0]
+        # else:
+        #     return 0
+
+
+        #Better
+
+        min_heap = []
+
+        for i in range(len(stones)):
+            stones[i] *= -1
+
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            stone1 = heapq.heappop(stones)
+            stone2 = heapq.heappop(stones)
+
+            if stone2!= stone1:
+                heapq.heappush(stones, stone1-stone2)
 
         if stones:
-            return stones[0]
+            return -heapq.heappop(stones)
+
         else:
             return 0
 
