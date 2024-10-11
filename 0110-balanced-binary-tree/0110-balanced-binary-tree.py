1ec659d0980d4c1e236f644e4acc1a19fe815d43
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-11 00:22:37 -0400

    Time: 43 ms (69.6%), Space: 17.6 MB (72.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,28 +6,58 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        #Brute O(n2)
+        # def get_height(root):
+        #     if root is None:
+        #         return 0
 
-        def get_height(root):
-            if root is None:
+        #     left = get_height(root.left)
+        #     right = get_height(root.right)
+
+
+        #     return 1 + max(left,right)
+
+
+        # if root is None:
+        #     return True
+
+        # left = get_height(root.left)
+        # right = get_height(root.right)
+
+
+        # if abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+        #     return True
+        
+        # return False
+
+        #Better
+
+        def dfsHeight(root):
+
+            if not root:
                 return 0
 
-            left = get_height(root.left)
-            right = get_height(root.right)
+            
+            leftHeight = dfsHeight(root.left)
+            
+            if leftHeight == -1:
+                return -1
 
+            
+            rightHeight = dfsHeight(root.right)
 
-            return 1 + max(left,right)
+            if rightHeight == -1:
+                return -1
 
+            
+            if abs(leftHeight - rightHeight) > 1:
+                return -1
 
-        if root is None:
-            return True
+            
+            return 1 + max(leftHeight, rightHeight) 
 
-        left = get_height(root.left)
-        right = get_height(root.right)
-
-
-        if abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
-            return True
         
-        return False
+        return dfsHeight(root) != -1
 
         
