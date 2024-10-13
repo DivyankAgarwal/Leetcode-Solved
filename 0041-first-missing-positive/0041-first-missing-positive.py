a4b6219b5067673e97d7c8b319a1e9f9e6de67ea
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-13 12:32:46 -0400

    Time: 369 ms (69.92%), Space: 30.2 MB (84.85%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,16 +2,61 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
         #brute
-        smp = 1
-        nums.sort()  #(n log n)
-        n = len(nums)
+        # smp = 1
+        # nums.sort()  #(n log n)
+        # n = len(nums)
 
-        for i in range(n):
-            if nums[i] == smp:
-                smp+=1
+        # for i in range(n):
+        #     if nums[i] == smp:
+        #         smp+=1
         
-        return smp
+        # return smp
 
 
         # Optimal
-        
+
+        # smp = 1
+        # if len(nums) == 0:
+        #     return smp
+
+
+        # i = 0
+        # j = len(nums) - 1
+
+        # while i<=j:
+
+
+        #     if nums[i] == smp:
+        #         smp+=1
+        #         i+=1
+        #     elif nums[j] == smp:
+        #         smp+=1
+        #         j-=1
+
+        #     elif nums[i] < smp:
+        #         i+=1
+
+        #     elif nums[j] < smp:
+        #         j-=1
+
+        #     else:
+        #         if nums[i] > len(nums):
+        #             i += 1
+        #         elif nums[j] > len(nums):
+        #             j -= 1
+        #         else:
+        #             i += 1
+
+        # return smp
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+
+
+        return n+1
