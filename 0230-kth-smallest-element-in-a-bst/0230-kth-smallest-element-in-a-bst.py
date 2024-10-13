6fde9edfdde3a53afa39868d66e995d5faf7a887
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-13 12:54:07 -0400

    Time: 46 ms (50.42%), Space: 19.6 MB (6.93%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,21 +7,41 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         #brute
-        def inorder(root, values): #left, root, right
-            if root is None:
+        # def inorder(root, values): #left, root, right
+        #     if root is None:
+        #         return
+
+        #     inorder(root.left, values)
+        #     values.append(root.val)
+        #     inorder(root.right, values)
+
+        # values = []
+        # inorder(root, values)
+
+        # values.sort()
+
+        # return values[k-1]
+
+
+        #optimal
+
+        self.k = k
+        self.result = None
+        
+        def inorder(root):
+            if not root:
                 return
-
-            inorder(root.left, values)
-            values.append(root.val)
-            inorder(root.right, values)
-
             
-
-
-        values = []
-        inorder(root, values)
-
-        values.sort()
-
-        return values[k-1]
+            inorder(root.left)
+            
+            self.k -= 1
+            if self.k == 0:
+                self.result = root.val
+                return
+        
+            inorder(root.right)
+        
+    
+        inorder(root)
+        return self.result
         
