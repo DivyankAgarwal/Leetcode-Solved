902533ad5018ce7bfb49bf6cb3fa54a0a8f898ba
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-15 13:20:13 -0400

    Time: 34 ms (76.57%), Space: 16.5 MB (84.22%) - LeetHub

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,55 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+
+        #brute
+
+        # def addOne(s):
+        #     #get intial pointer pointing to last
+
+        #     pointer = len(s) - 1
+        #     slist = list(s)
+
+        #     while pointer >= 0 and slist[pointer] != "1":
+        #         slist[pointer] = "0"
+        #         pointer -=1
+
+        #     if pointer == -1:
+        #         slist = ["1"] + slist
+
+        #     else:
+        #         slist[pointer] = "1"
+
+        #     return "".join(slist)
+
+
+        # steps = 0
+        # while s!= "1":
+        #     #in binary, the last digit determines if no is even  or not
+
+        #     if s[-1] == "0":
+        #         s = s[:-1]
+
+        #     else:
+        #         s = addOne(s)
+
+        #     steps +=1
+
+        # return steps
+
+
+        #optimal.
+        
+        
+        num = int(s, 2)  
+        steps = 0
+
+       
+        while num > 1:
+            if num % 2 == 0:  
+                num //= 2     
+            else:
+                num += 1      
+            steps += 1        
+
+        return steps
+        
