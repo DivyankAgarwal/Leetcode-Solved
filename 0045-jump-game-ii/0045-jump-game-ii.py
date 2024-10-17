4c75c292ad5542907a9c27ee8c2f2bfc982623a5
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-17 00:04:25 -0400

    Time: 114 ms (36.2%), Space: 17.6 MB (83.58%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -16,19 +16,45 @@ class Solution:
 
         # return jump_helper(0,0)
 
-        #Better
+        #Better TC: O(n**2)
+
+        # n = len(nums)
+        # dp = [float('inf')] * n  
+
+        # dp[0] = 0  
+
+        # for i in range(1, n):
+        #     for j in range(i):
+
+        #         if j + nums[j] >= i: 
+        #             dp[i] = min(dp[i], dp[j] + 1)  
+
+        # return dp[-1]  
+
+
+        #Optimal O(N)
 
         n = len(nums)
-        dp = [float('inf')] * n  
-
-        dp[0] = 0  
-
-        for i in range(1, n):
-            for j in range(i):
+    
+        if n == 1:  
+            return 0
+        
+        jumps = 0
+        current_jump_end = 0
+        farthest = 0
+        
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])  
+            
+       
+            if i == current_jump_end:
+                jumps += 1
+                current_jump_end = farthest  
                 
-                if j + nums[j] >= i: 
-                    dp[i] = min(dp[i], dp[j] + 1)  
 
-        return dp[-1]  #
+                if current_jump_end >= n - 1:
+                    break
+        
+        return jumps
         
         
