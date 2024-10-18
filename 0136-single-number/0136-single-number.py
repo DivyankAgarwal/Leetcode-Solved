28ad8d846d5d5948b7a64acbf033a2dcef88546c
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-18 13:35:13 -0400

    Time: 2 ms (100%), Space: 18.7 MB (99.36%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,16 +2,26 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
         #brute
-        d = defaultdict(int)
+        # d = defaultdict(int)
+
+        # for n in nums:
+        #     d[n] +=1
+
+        # for k,v in d.items():
+        #     if v == 1:
+        #         return k
+        
+        # return -1
+
+
+        #Space O(1)
+
+        result = 0
 
         for n in nums:
-            d[n] +=1
+            result = result ^ n
 
-        for k,v in d.items():
-            if v == 1:
-                return k
-        
-        return -1
+        return result
 
         
         
