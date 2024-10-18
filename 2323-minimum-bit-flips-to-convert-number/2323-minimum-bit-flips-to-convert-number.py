a550930f549fb3f47eaba261dde6c2fd1cdedb3b
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-18 14:04:56 -0400

    Time: 0 ms (100%), Space: 16.5 MB (33.46%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,15 +1,28 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
 
+        # difference = start ^ goal
+
+        # print(difference)
+        # count  = 0
+
+        # while difference:
+        #     count+= difference & 1
+
+        #     difference = difference >> 1
+
+        # return count
+
+        #optimal O(1)
+
         difference = start ^ goal
 
-        print(difference)
-        count  = 0
+        count = 0
 
-        while difference:
-            count+= difference & 1
+        for i in range(32):
+            count += difference & 1
 
-            difference = difference >> 1
+            difference  =  difference >> 1
 
         return count
         
