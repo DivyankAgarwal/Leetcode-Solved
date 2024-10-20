b41c09dac5aa02fcbb9b13e103996906942dbee1
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-20 15:29:26 -0400

    Time: 223 ms (98.85%), Space: 21.1 MB (8.77%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,57 @@
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+
+
+        def isValid(row,col):
+            if row < 0 or row >= len(mat):
+                return False
+
+            if col < 0 or col >= len(mat[0]):
+                return False
+
+            return True
+
+        distance = [ [0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+
+        visited = [ [0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+
+        print(distance)
+
+        #bfs
+        q = deque()
+
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+
+                if mat[i][j] == 0:
+                    q.append([(i,j),0])
+                    visited[i][j] = 1
+
+        deltaRows = [-1,0,1,0]
+        deltaCol = [0,1,0,-1]
+
+        while q:
+
+            for i in range(len(q)):
+
+                data = q.popleft()
+                row,col = data[0]
+                step = data[1]
+                distance[row][col] = step
+
+                for i in range(4):
+                    nR = row + deltaRows[i]
+                    nC = col + deltaCol[i]
+
+                    if isValid(nR, nC) and visited[nR][nC] == 0:
+
+                        visited[nR][nC] = 1
+                        q.append([(nR,nC), step+1])
+        
+        return distance
+
+
+
+        
