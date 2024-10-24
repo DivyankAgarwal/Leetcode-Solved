846801e69791cdca7029e7b721843ca7ab2aea91
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-24 16:10:33 -0400

    Time: 0 ms (100%), Space: 17 MB (46.6%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -1,32 +1,56 @@
+import heapq
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
 
         #brute
+        # student_score = collections.defaultdict(list)
+
+        # for id, score in items:
+        #     student_score[id].append(score)
+
+        # result = []
+
+        # for id in student_score.keys():
+
+        #     scores = student_score[id]
+
+        #     scores.sort(reverse = True)
+
+        #     top5scores = scores[:5]
+
+        #     avg = sum(top5scores) // 5
+
+        #     result.append([id, avg])
+
+        # result.sort(key = lambda x: x[0])
+
+        # return result
+
+        # Optimized - using heap
+
+
         student_score = collections.defaultdict(list)
 
         for id, score in items:
-            student_score[id].append(score)
+            heapq.heappush(student_score[id], score)
+            if len(student_score[id]) > 5:
+                heapq.heappop(student_score[id])
+
+            
+            
+        print(student_score)
 
         result = []
+        
 
-        for id in student_score.keys():
-
-            scores = student_score[id]
-
-            scores.sort(reverse = True)
-
-            top5scores = scores[:5]
-
-            avg = sum(top5scores) // 5
-
-            result.append([id, avg])
-
-        result.sort(key = lambda x: x[0])
+        for id, score in sorted(student_score.items()):
+           
+            avg = sum(score) // len(score)
+            result.append([id,avg])
 
         return result
 
-        
 
-        return result.sort(key= lambda x: x[0])
+
 
         
