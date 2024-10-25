91e2fb9151734193178a3408592d5f3fb0e8b842
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-25 18:38:06 -0400

    Time: 37 ms (89.8%), Space: 23.9 MB (10.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,30 +2,50 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         #Brute
+        '''
+        Time Complexity:
+        O(n * k log k), where n is the number of strings, and k is the maximum length of a 
+        string (since we sort each string).
 
-        anagrams = defaultdict(list)  
+        Space Complexity:
+        O(n * k), as we store each string in the dictionary.
         
-        for word in strs:
-            sorted_word = ''.join(sorted(word))  
-            anagrams[sorted_word].append(word) 
+    
+        '''
+
+        # anagrams = defaultdict(list)  
         
-        return list(anagrams.values())
+        # for word in strs:
+        #     sorted_word = ''.join(sorted(word))  
+        #     anagrams[sorted_word].append(word) 
+        
+        # return list(anagrams.values())
 
 
         #optimal
-
-        # anagrams = defaultdict(list)  
-
-        # for word in strs:
-        #     count = [0] * 26 
-            
-        #     for char in word:
-        #         count[ord(char) - ord('a')] += 1  
-        #     print(count)
-            
-        #     anagrams[tuple(count)].append(word)  # Use tuple of counts as the key
-
-
+        '''
+        Time Complexity:
+        O(n * k): For each word of length k, we build the character count array in O(k), 
+        making the complexity O(n * k).
         
-        # return list(anagrams.values()) 
+        Space Complexity:
+        O(n * k): We store each string in the dictionary, and each key (tuple) has a fixed 
+        length of 26 (for each letter in the alphabet).
+        
+        '''
+
+        anagrams = defaultdict(list)  
+
+        for word in strs:
+            count = [0] * 26 
+            
+            for char in word:
+                count[ord(char) - ord('a')] += 1  
+
+            
+            anagrams[tuple(count)].append(word)  # Use tuple of counts as the key
+
+
+        print(anagrams)
+        return list(anagrams.values()) 
         
