574a08994d3af71f6f27d7e929d9d5a534b98143
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-26 22:36:08 -0400

    Time: 275 ms (68.05%), Space: 16.7 MB (28.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,18 +1,47 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        #brute
-        def check(substring):
-            return substring == substring[::-1]
+        #brute TC: O(n3)
+        # def check(substring):
+        #     return substring == substring[::-1]
 
-        longest = ''
+        # longest = ''
+
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         substring = s[i:j+1]
+        #         if check(substring) and len(substring) > len(longest):
+        #             longest = substring
+
+        
+        # return longest
+        
+        #better
+
+        result = ''
+
+        resultlength = 0
 
         for i in range(len(s)):
-            for j in range(i, len(s)):
-                substring = s[i:j+1]
-                if check(substring) and len(substring) > len(longest):
-                    longest = substring
 
-        
-        return longest
-        
+            left,right = i,i
+            #odd length
+            while left >=0 and right < len(s) and s[left] == s[right]:
+                if (right-left + 1) > resultlength:
+                    result = s[left:right+1]
+                    resultlength = (right-left+1)
+                left -=1
+                right +=1
+
+            #even length
+            left = i
+            right = i+1
+
+            while left >=0 and right < len(s) and s[left] == s[right]:
+                if (right-left + 1) > resultlength:
+                    result = s[left:right+1]
+                    resultlength = (right-left+1)
+                left -=1
+                right +=1
+
+        return result
