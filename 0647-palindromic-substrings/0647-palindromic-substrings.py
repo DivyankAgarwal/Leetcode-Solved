298945c9eb22d8b1d59db382e333ba020c99f7c0
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-26 21:08:19 -0400

    Time: 102 ms (91.26%), Space: 16.6 MB (36.57%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,19 +1,47 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
 
-        #brute
+        #brute TC O(n3)
 
-        def check_if_palindrome(s):
-            return s == s[::-1]
+        # def check_if_palindrome(s):
+        #     return s == s[::-1]
 
-        count = 0
+        # count = 0
+        # n = len(s)
+
+        # for i in range(n):
+        #     for j in range(i,n):
+        #         if check_if_palindrome(s[i:j+1]):
+        #             count+=1
+
+
+        # return count
+
+
+        #optimized
+
+
+        def check_around_center(left, right):
+
+            nonlocal count
+
+            while left >=0 and right < n and s[left] == s[right]:
+                count+=1
+                left -=1
+                right +=1
+
+
         n = len(s)
+        count = 0
 
         for i in range(n):
-            for j in range(i,n):
-                if check_if_palindrome(s[i:j+1]):
-                    count+=1
 
+            check_around_center(i,i)
+
+
+            check_around_center(i,i+1)
 
         return count
+
+
         
