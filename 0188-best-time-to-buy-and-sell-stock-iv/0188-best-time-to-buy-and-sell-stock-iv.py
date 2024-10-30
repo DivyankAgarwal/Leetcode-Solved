bbd39574b0f7c497ca4dd393a95463d4e09d27f1
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-10-29 20:54:34 -0400

    Time: 99 ms (43.17%), Space: 24.8 MB (38.15%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,28 +1,52 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        def calculate(index, buy, cap,dp):
-            if index == n or cap == 0:
-                return 0
+        #memoization
+        # def calculate(index, buy, cap,dp):
+        #     if index == n or cap == 0:
+        #         return 0
 
-            if dp[index][buy][cap] != -1:
-                return dp[index][buy][cap]
+        #     if dp[index][buy][cap] != -1:
+        #         return dp[index][buy][cap]
 
-            if buy == 0:
-                profit = max(0 + calculate(index+1, 0, cap,dp), -1*prices[index] + calculate(index+1, 1, cap,dp))
-
-
-
-            elif buy == 1:
-                profit = max(0 + calculate(index+1, 1, cap,dp), prices[index] + calculate(index+1, 0, cap-1,dp))
+        #     if buy == 0:
+        #         profit = max(0 + calculate(index+1, 0, cap,dp), -1*prices[index] + calculate(index+1, 1, cap,dp))
 
 
-            dp[index][buy][cap] = profit
 
-            return dp[index][buy][cap]
+        #     elif buy == 1:
+        #         profit = max(0 + calculate(index+1, 1, cap,dp), prices[index] + calculate(index+1, 0, cap-1,dp))
 
-        n = len(prices)
-        dp = [[[-1 for _ in range(k+1)]for _ in range(2)]for _ in range(n)]
+
+        #     dp[index][buy][cap] = profit
+
+        #     return dp[index][buy][cap]
+
+        # n = len(prices)
+        # dp = [[[-1 for _ in range(k+1)]for _ in range(2)]for _ in range(n)]
 
     
-        return calculate(0, 0, k,dp)
+        # return calculate(0, 0, k,dp)
         
+        #tabulation
+        n = len(prices)
+        dp = [[[0 for _ in range(k + 1)] for _ in range(2)] for _ in range(n + 1)]
+
+        for index in reversed(range(n)):
+
+            #2 options buy and sell
+
+            for buy in range(2):
+
+                #capacity
+
+                for cap in range(1, k+1):
+
+                    if buy == 0:
+                        dp[index][buy][cap] = max(0 + dp[index+1][0][cap], -1*prices[index] + dp[index+1][1][cap])
+
+
+                    elif buy == 1:
+                        dp[index][buy][cap] = max(0 + dp[index+1][1][cap], prices[index] + dp[index+1][0][cap-1])
+
+
+        return dp[0][0][k]
