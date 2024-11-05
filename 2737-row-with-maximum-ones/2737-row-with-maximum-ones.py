02259cdc1abe3cb6a4ee3d40b0aeb5526a6ecf02
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-11-05 12:33:55 -0500

    Time: 23 ms (33.91%), Space: 17.5 MB (11.76%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -1,20 +1,39 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        result = []
-        count1 = 0
+        #naive
+        # result = []
+        # count1 = 0
+
+        # for index, data in enumerate(mat):
+        #     count1 = 0
+        #     for entry in data:
+        #         if entry == 1:
+        #             count1 +=1
+            
+        #     if count1 != 0:
+        #         result.append([index,count1])
+
+        # if all(count == 0 for _, count in result):
+        #     return [0, 0]
+
+
+        # result.sort(key=lambda x: (-x[1], x[0]))
+        # return result[0]
+
+        #remove sorting.
+        max_count = 0
+        max_row_index = 0
 
         for index, data in enumerate(mat):
             count1 = 0
             for entry in data:
                 if entry == 1:
                     count1 +=1
-            
-            if count1 != 0:
-                result.append([index,count1])
 
-        if all(count == 0 for _, count in result):
-            return [0, 0]
+            if count1 > max_count:
+                    max_count = count1
+                    max_row_index = index
+            elif count1 == max_count and index < max_row_index:
+                max_row_index = index
 
-
-        result.sort(key=lambda x: (-x[1], x[0]))
-        return result[0]
+        return [max_row_index, max_count]
