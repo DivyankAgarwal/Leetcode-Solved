fff3d88ec7119beec613461a6907b3ada1e7e745
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-11-12 12:25:20 -0500

    Time: 2 ms (60.99%), Space: 16.7 MB (40.54%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,6 +1,39 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         
+        # n = len(s)
+        # d = Counter(s)
+
+        # max_val = max(d.values())
+        # print(max_val)
+
+        # if max_val > (n+1) // 2:
+        #     return ""
+
+        # sorted_chars = sorted(d.keys(), key=lambda x: -d[x])
+
+        # ans = [''] * len(s)
+
+        # print(sorted_chars)
+        # index = 0
+        
+        # for char in sorted_chars:
+        #     freq = d[char]
+
+        #     for _ in range(freq):
+
+        #         if index >= len(s):
+        #             index = 1
+
+        #         ans[index] = char
+        #         index+=2
+        
+
+        # return ''.join(ans)
+
+
+        #using max heap
+
         n = len(s)
         d = Counter(s)
 
@@ -9,27 +42,29 @@ class Solution:
 
         if max_val > (n+1) // 2:
             return ""
-
-        sorted_chars = sorted(d.keys(), key=lambda x: -d[x])
-
-        ans = [''] * len(s)
-
-        print(sorted_chars)
-        index = 0
         
-        for char in sorted_chars:
-            freq = d[char]
+        max_heap = [(-freq, char) for char, freq in d.items()]
+        heapq.heapify(max_heap)
+        ans = []
+        prev_char = ''
+        prev_count = 0
+        while max_heap:
 
-            for _ in range(freq):
+            count,char = heapq.heappop(max_heap)
 
-                if index >= len(s):
-                    index = 1
+            ans.append(char)
+            
+            if prev_count < 0:
+                heapq.heappush(max_heap, (prev_count, prev_char))
 
-                ans[index] = char
-                index+=2
+            # Update previous character and frequency (decrease current character's frequency)
+            prev_char, prev_count = char, count + 1
+
         
-
         return ''.join(ans)
+
+
+
 
 
 
