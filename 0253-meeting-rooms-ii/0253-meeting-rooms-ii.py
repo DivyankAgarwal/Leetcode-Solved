f9b83a975cf5595d2e833ac3f83c68c25c648810
Divyank Agarwal <53295012+DivyankAgarwal@users.noreply.github.com>
2024-11-12 15:34:18 -0500

    Time: 3 ms (84.36%), Space: 18.9 MB (85.62%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,25 +1,59 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 
-        start = []
-        end = []
-        for s, e in intervals:
-            start.append(s)
-            end.append(e)
+        '''
+        The main objective is to determine how many meetings are ongoing at any point in 
+        time to understand the peak room usage. We don’t actually need to know which specific 
+        start time pairs with which end time. Instead, we simply need to know:
 
-        start.sort()
-        end.sort()
-        rooms = 0
+        When each meeting begins.
+        When each meeting ends.
+        
+        '''
 
-        end_ptr = 0
+        # start = []
+        # end = []
+        # for s, e in intervals:
+        #     start.append(s)
+        #     end.append(e)
 
-        for i in range(len(start)):
-            if start[i] < end[end_ptr]:
-                rooms+=1
-            else:
-                end_ptr+=1
+        # start.sort()
+        # end.sort()
+        # rooms = 0
 
-        return rooms
+        # end_ptr = 0
+
+        # for i in range(len(start)):
+        #     if start[i] < end[end_ptr]:
+        #         rooms+=1
+        #     else:
+        #         end_ptr+=1
+
+        # return rooms
+
+        #using min_heap
+
+        if not intervals:
+            return 0
+
+       
+        intervals.sort(key=lambda x: x[0])
+
+        
+        min_heap = []
+        heapq.heappush(min_heap, intervals[0][1])  
+
+       
+        for i in range(1, len(intervals)):
+           
+            if min_heap[0] <= intervals[i][0]:
+                heapq.heappop(min_heap)  
+
+            # Add the current meeting's end time to the heap
+            heapq.heappush(min_heap, intervals[i][1])
+
+        
+        return len(min_heap)
 
             
 
